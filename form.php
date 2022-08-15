fe59a5e83f3ff8c8109e4c3c534115bf4a5c7370
luana <luanaesteve@hotmail.com>
2022-08-15 18:08:24 -0300

    adfafafa

--- form.php
+++ form.php
@@ -1,4 +1,13 @@
 <?php
-if ($_POST){
-    $cantidad_hijos = $_POST["Hijos"];
+$pregunta_hijos = $_GET['hijos'];
+$cantidad_hijos = $_GET['cantidad'];
+$casamiento = $_GET['casamiento'];
+$patagonia = $_GET['patagonia'];
+$submit = $_GET['submit'];
+
+if ($_REQUEST['hijos'] == "si") {
+    $pregunta_hijos = $_POST["hijos"];
+    echo $_REQUEST['hijos']."si tiene hijos"; 
 }
+
+?>
